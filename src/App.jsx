6961f4dcee8d6936d15d84c17c5b5a1df31318a4
Tanmay Agrawal <tanmayagcode@gmail.com>
2024-08-25 01:26:44 +0530

    added hash router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 // import { lightTheme, darkTheme } from "./theme";
 
 import Layout from "./layout";
